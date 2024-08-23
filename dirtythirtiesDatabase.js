02130ea56ed5e986222e49b6223252e4fce3fd29
Svetlana Dubkova <svetl@MacBook-Air.local>
2024-08-23 18:11:16 -0400

    new bot

--- dirtythirtiesDatabase.js
+++ dirtythirtiesDatabase.js
@@ -4,9 +4,9 @@ module.exports = {
     plusOneResponse: "umm yes if they're nice and fun only",
     partyLocationResponse: "rsvp for the location :)",
     speedDatingResponse: "lana hasn't had time to date so she's inviting a bunch of people from hinge for a fun speed dating thing as a low-pressure / low-commit way to meet people irl. you'll get to meet these people and also vicariously date them lol",
-    eventTimeResponse: "oh snap, you're in for a treat! the big 3-0 bash is going down on saturday, june 15th, 2024. mark your calendars, set those phone reminders, do whatever you gotta do bc this is gonna be one for the books. we're kicking off the dirty thirties in style, so you better be ready to party like it's 1994 (which, let's be real, most of us can't even remember). it's gonna be lit af and you do NOT want to miss out on this epic night of questionable decisions and unparalleled awesomeness.",
+    eventTimeResponse: "friday, september 20th, 2024 at 7pm. mark your calendars, set those phone reminders, rsvp so we can order enough food and stuff.",
     eventDetails: {
-        date: "June 15, 2024",
+        date: "September 20, 2024",
         time: "7:00 PM",
         rsvpLink: "https://lu.ma/event/evt-LIZKqDTWv3EElMF"
     },
